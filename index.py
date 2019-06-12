7dac523052368c199b049da14032a766883b3e5e
Jeevan <waterloodevs@gmail.com>
2019-06-12 19:47:13 -0400

    gh

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import asyncio
 import kin
 import smtplib
+import psycopg2
 from email.message import EmailMessage
 from flask import Flask, render_template, request, redirect, url_for, abort, g, Response
 from flask import jsonify
@@ -11,18 +12,32 @@ from firebase_admin import credentials, auth, messaging
 from flask_httpauth import HTTPTokenAuth
 import gunicorn
 
-http_auth = HTTPTokenAuth(scheme='Token')
 
 cred = credentials.Certificate('kino-extension-firebase-adminsdk-mrn7d-cca5ca5e4c.json')
 default_app = firebase_admin.initialize_app(cred)
+http_auth = HTTPTokenAuth(scheme='Token')
+kin_env = kin.TEST_ENVIRONMENT
+app_id = '9999'
+kino_seed = ''
+kino_public_address = ''
+database_url = 'postgres://aueqysferuszbo:8eb6155fc340e1de1b38ce4eac1aa190f15bbc527ee52ce389612b1bde6d14b6@ec2-107-22-238-217.compute-1.amazonaws.com:5432/ddrnuulbba6300'
 
-DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/postgres'
 
 app = Flask(__name__)
-# run_with_ngrok(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://aueqysferuszbo:8eb6155fc340e1de1b38ce4eac1aa190f15bbc527ee52ce389612b1bde6d14b6@ec2-107-22-238-217.compute-1.amazonaws.com:5432/ddrnuulbba6300'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+affiliate_links = {
+        "www.berrylook.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=44303&murl=https%3A%2F%2Fwww.berrylook.com%2F",
+        "www.bloomstoday.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=44085&murl=http%3A%2F%2Fwww.bloomstoday.com",
+        "www.cheapoair.ca": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=37732&murl=https%3A%2F%2Fwww.cheapoair.ca%2F",
+        "www.josbank.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=38377&murl=http%3A%2F%2Fwww.josbank.com",
+        "www.shop.lego.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=13923&murl=http%3A%2F%2Fshop.lego.com",
+        "www.macys.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=3184&murl=http%3A%2F%2Fwww.macys.com",
+        "www.microsoft.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=24542&murl=http%3A%2F%2Fwww.microsoft.com"
+}
 
 
 class User(db.Model):
@@ -37,7 +52,7 @@ class User(db.Model):
     android_fcm_token = db.Column(db.String)
     public_address = db.Column(db.String)
 
-    def __init__(self, uid, email, balance=0, transactions=None,
+    def __init__(self, uid, email, balance=0, transactions=[],
                  android_fcm_token=None, web_fcm_token=None, public_address=None):
         self.uid = uid
         self.email = email
@@ -59,84 +74,130 @@ def verify_token(fb_id_token):
     return True
 
 
-@app.route('/', methods=['GET'])
-@http_auth.login_required
-def index():
-    return "hello"
+def get_database_connection():
+    conn = psycopg2.connect(database_url, sslmode='allow')
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    return conn, cur
+
+
+async def init_kin():
+    client = kin.KinClient(kin_env)
+    account = client.kin_account(kino_seed, app_id=app_id)
+    return client, account
 
 
 @app.route('/register', methods=['POST'])
 @http_auth.login_required
 def register():
-    email = str(auth.get_user(g.uid).email)
-    user = User(uid=g.uid, email=email)
-    db.session.add(user)
+    conn, cur = get_database_connection()
     try:
-        db.session.commit()
-    except AssertionError as err:
-        db.session.rollback()
-
-    return jsonify(), 201
+        email = str(auth.get_user(g.uid).email)
+        user = User(uid=g.uid, email=email)
+        cur.execute(
+            """
+            INSERT into \"users\" 
+                (uid, email, balance, transactions, web_fcm_token, android_fcm_token, public_address)
+            VALUES
+                (%s, %s, %s, %s, %s, %s, %s)
+            """,
+            [user.uid, user.email, user.balance, user.transactions,
+             user.web_fcm_token, user.android_fcm_token, user.public_address]
+        )
+        conn.commit()
+        return jsonify(), 201
+    except:
+        return jsonify(), 500
+    finally:
+        cur.close()
+        conn.close()
 
 
 @app.route('/update_fcm_token', methods=['POST'])
 @http_auth.login_required
 def update_fcm_token():
-    user = User.query.filter_by(uid=g.uid).first()
-    if 'web_fcm_token' in request.json:
-        web_fcm_token = request.json['web_fcm_token']
-        user.web_fcm_token = web_fcm_token
-    elif 'android_fcm_token' in request.json:
-        android_fcm_token = request.json['android_fcm_token']
-        user.android_fcm_token = android_fcm_token
-    else:
-        return jsonify(), 500
+    conn, cur = get_database_connection()
     try:
-        db.session.commit()
-    except AssertionError as err:
-        db.session.rollback()
-        return jsonify(), 500
-    return jsonify(), 201
-
-
-async def onboard_account_async(request):
-
-    KINO_PRIVATE_ADDRESS = "SCTDXS2NGAKNMSJFJVEE7IQQDUM7KAXTHNRVFTXRGMUYNFIO6LJ5U5M6"
-    APP_ID = "1111"
-
-    if 'public_address' not in request.json:
-        return jsonify(), 500
-    public_address = request.json['public_address']
-    user = User.query.filter_by(uid=g.uid).first()
-    client = kin.KinClient(kin.TEST_ENVIRONMENT)
-    try:
-        account = client.kin_account(KINO_PRIVATE_ADDRESS, app_id=APP_ID)
-        fee = await client.get_minimum_fee()
-        tx_hash = await account.create_account(public_address, 100, fee=fee, memo_text='create account')
+        if 'web_fcm_token' in request.json:
+            web_fcm_token = request.json['web_fcm_token']
+            cur.execute(
+                """
+                UPDATE \"users\" 
+                SET
+                    web_fcm_token = %s
+                WHERE   
+                    uid = %s
+                """,
+                [web_fcm_token, g.uid]
+            )
+            conn.commit()
+        elif 'android_fcm_token' in request.json:
+            android_fcm_token = request.json['android_fcm_token']
+            cur.execute(
+                """
+                UPDATE \"users\" 
+                SET
+                    android_fcm_token = %s
+                WHERE   
+                    uid = %s
+                """,
+                [android_fcm_token, g.uid]
+            )
+            conn.commit()
+        else:
+            return jsonify(), 500
+        return jsonify(), 201
     except:
         return jsonify(), 500
     finally:
-        await client.close()
-    first_time = True
-    if user.public_address:
-        first_time = False
-    user.public_address = public_address
+        cur.close()
+        conn.close()
+
+
+async def onboard_account_async(json):
+    conn, cur = get_database_connection()
+    client, account = await init_kin()
     try:
-        db.session.commit()
-    except AssertionError as err:
-        db.session.rollback()
-        return jsonify(), 500
-    # First time installing the app and balance is not 0, create a earn transaction for the user's balance
-    if first_time and user.balance:
-        try:
-            account = client.kin_account(KINO_PRIVATE_ADDRESS, app_id=APP_ID)
+        public_address = json['public_address']
+        cur.execute(
+            """
+            SELECT
+                *
+            FROM
+                \"users\"
+            WHERE
+                uid = %s
+            """,
+            [g.uid]
+        )
+        user = cur.fetchone()
+        account = client.kin_account(kino_seed, app_id=app_id)
+        fee = await client.get_minimum_fee()
+        tx_hash = await account.create_account(public_address, 100, fee=fee, memo_text='create account')
+        first_time = True
+        if user['public_address']:
+            first_time = False
+        cur.execute(
+            """
+            UPDATE \"users\" 
+            SET
+                public_address = %s
+            WHERE   
+                uid = %s
+            """,
+            [public_address, g.uid]
+        )
+        conn.commit()
+        # First time installing the app and balance is not 0, create a earn transaction for the user's balance
+        if first_time and user['balance'] != 0:
             fee = await client.get_minimum_fee()
-            tx_hash = await account.send_kin(user.public_address, user.balance, fee=fee, memo_text='balance')
-        except:
-            return jsonify(), 500
-        finally:
-            await client.close()
-    return jsonify(), 201
+            tx_hash = await account.send_kin(user['public_address'], user['balance'], fee=fee, memo_text='balance')
+        return jsonify(), 201
+    except:
+        return jsonify(), 500
+    finally:
+        cur.close()
+        conn.close()
+        await client.close()
 
 
 @app.route('/onboard_account', methods=['POST'])
@@ -144,41 +205,41 @@ async def onboard_account_async(request):
 def onboard_account():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    response = loop.run_until_complete(onboard_account_async(request))
+    response = loop.run_until_complete(onboard_account_async(request.get_json()))
     return response
 
 
 @app.route('/stores', methods=['GET'])
 @http_auth.login_required
 def stores():
-    AFFILIATE_LINKS = {
-        "www.berrylook.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=44303&murl=https%3A%2F%2Fwww.berrylook.com%2F",
-        "www.bloomstoday.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=44085&murl=http%3A%2F%2Fwww.bloomstoday.com",
-        "www.cheapoair.ca": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=37732&murl=https%3A%2F%2Fwww.cheapoair.ca%2F",
-        "www.josbank.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=38377&murl=http%3A%2F%2Fwww.josbank.com",
-        "www.shop.lego.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=13923&murl=http%3A%2F%2Fshop.lego.com",
-        "www.macys.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=3184&murl=http%3A%2F%2Fwww.macys.com",
-        "www.microsoft.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=24542&murl=http%3A%2F%2Fwww.microsoft.com"
-    }
-    return jsonify({"stores": list(AFFILIATE_LINKS.keys())}), 200
+    return jsonify({"stores": list(affiliate_links.keys())}), 200
 
 
 @app.route('/affiliate_link/<url>', methods=['GET'])
 @http_auth.login_required
 def affiliate_link(url):
-    AFFILIATE_LINKS = {
-        "www.berrylook.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=44303&murl=https%3A%2F%2Fwww.berrylook.com%2F",
-        "www.bloomstoday.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=44085&murl=http%3A%2F%2Fwww.bloomstoday.com",
-        "www.cheapoair.ca": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=37732&murl=https%3A%2F%2Fwww.cheapoair.ca%2F",
-        "www.josbank.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=38377&murl=http%3A%2F%2Fwww.josbank.com",
-        "www.shop.lego.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=13923&murl=http%3A%2F%2Fshop.lego.com",
-        "www.macys.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=3184&murl=http%3A%2F%2Fwww.macys.com",
-        "www.microsoft.com": "https://click.linksynergy.com/deeplink?id=fw7GSdQ4wUE&mid=24542&murl=http%3A%2F%2Fwww.microsoft.com"
-    }
-    user = User.query.filter_by(uid=g.uid).first()
-    link = AFFILIATE_LINKS[url]
-    # Every url needs to have http(s):// at the start
-    link += "&u1=" + user.uid
+    conn, cur = get_database_connection()
+    try:
+        cur.execute(
+            """
+            SELECT
+                *
+            FROM
+                \"users\"
+            WHERE
+                uid = %s
+            """,
+            [g.uid]
+        )
+        user = cur.fetchone()
+        link = affiliate_links[url]
+        # Every url needs to have http(s):// at the start
+        link += "&u1=" + user['uid']
+    except:
+        return jsonify(), 500
+    finally:
+        cur.close()
+        conn.close()
     return jsonify({"url": link}), 200
 
 
@@ -248,32 +309,34 @@ def valid_order(order):
     return True
 
 
-async def buy_giftcard_async(request):
-    KINO_PUBLIC_ADDRESS = "GCFEL2AKYJFFNSPDA6CVCD3T3BHD6LZBQLGT5RYP4ZT6WAH42PZ5YNZN"
-    KINO_PRIVATE_ADDRESS = "SCTDXS2NGAKNMSJFJVEE7IQQDUM7KAXTHNRVFTXRGMUYNFIO6LJ5U5M6"
-    APP_ID = "1111"
+async def buy_giftcard_async(json):
     user = None
-    client = kin.KinClient(kin.TEST_ENVIRONMENT)
+    conn, cur = get_database_connection()
+    client, account = await init_kin()
     try:
-        user = User.query.filter_by(uid=g.uid).first()
-        order = request.json
+        cur.execute(
+            """
+            SELECT 
+                *
+            FROM 
+                \"users\"
+            WHERE   
+                uid = %s
+            """,
+            [g.uid]
+        )
+        user = cur.fetchone()
+        order = json
         if not valid_order(order):
             error = "Order validation failed"
             return jsonify(), 500
-        # Get gift-card type, amount, email, and quantity
-        type_ = order['type']
-        email = order['email']
-        amount = int(order['amount'])
-        quanity = int(order['quantity'])
         total = float(order['total'])
         envelope = order['envelope']
-
         network_id = order['network_id']
         data = kin.decode_transaction(envelope, network_id)
-        if data.operation.destination != KINO_PUBLIC_ADDRESS:
+        if data.operation.destination != kino_public_address:
             error = "Spend transaction to the wrong public address (not kino's)"
             return jsonify(), 500
-        account = client.kin_account(KINO_PRIVATE_ADDRESS, app_id=APP_ID)
         whitelisted_tx = account.whitelist_transaction(
             {"envelope": envelope,
              "network_id": network_id})
@@ -287,6 +350,8 @@ async def buy_giftcard_async(request):
             error = "Spend transaction failed"
         return jsonify(), 500
     finally:
+        cur.close()
+        conn.close()
         await client.close()
         if user:
             email_kino(user, spend_transaction=request.json, error=error)
@@ -297,7 +362,7 @@ async def buy_giftcard_async(request):
 def buy_giftcard():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    response = loop.run_until_complete(buy_giftcard_async(request))
+    response = loop.run_until_complete(buy_giftcard_async(request.get_json()))
     return response
 
 
